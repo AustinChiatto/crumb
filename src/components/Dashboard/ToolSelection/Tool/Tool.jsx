2816544bc2a3c27942cf1c3158cb38c592d08817
Austin Chiatto <chiattoaustin@gmail.com>
2023-07-13 21:43:27 -0700

    added a tool bar with routes to correct tool

--- src/components/Dashboard/ToolSelection/Tool/Tool.jsx
+++ src/components/Dashboard/ToolSelection/Tool/Tool.jsx
@@ -1,11 +1,18 @@
 import styles from "./Tool.module.scss";
+import gridTool from "../../../../assets/images/grid-tool-thumbnail.png";
 
 export default function Tool() {
     return (
         <li className={styles.Tool}>
             <a href={`/grid-tool`}>
                 <h3 className="text-base">Tool Heading</h3>
-                <div className={styles.ToolThumbnail}>grid tool</div>
+                <div className={styles.ToolThumbnail}>
+                    <img
+                        className={styles.ToolThumbnailImg}
+                        src={gridTool}
+                        alt=""
+                    />
+                </div>
             </a>
         </li>
     );
