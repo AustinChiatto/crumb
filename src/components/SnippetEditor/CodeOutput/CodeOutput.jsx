5793f81a3faa0340e911cc664b05e7a75f01b13f
Austin Chiatto <chiattoaustin@gmail.com>
2023-07-14 21:27:54 -0700

    linked code & snippet output to snippet context

--- src/components/SnippetEditor/CodeOutput/CodeOutput.jsx
+++ src/components/SnippetEditor/CodeOutput/CodeOutput.jsx
@@ -1,3 +1,6 @@
+// context
+import { useSnippetEdit } from "../../../SnippetEditorContext";
+
 // components
 import ButtonPrimary from "../../global/ButtonPrimary/ButtonPrimary";
 import CodeBlock from "../../global/CodeBlock/CodeBlock";
@@ -6,16 +9,20 @@ import CodeBlock from "../../global/CodeBlock/CodeBlock";
 import styles from "./CodeOutput.module.scss";
 
 export default function CodeOutput() {
+    // context that hold input values
+    const inputValues = useSnippetEdit();
+
     let codeSnippet = `
     // base style
     .button {
-            color: #fff;
-            padding-block: 16px;
-            padding-inline: 32px;
-            background: #974ec2;
-            box-shadow: 0px 3px 0px 0px #513f99;
-            border-radius: 8px
-            transition: all 50ms ease-in-out;
+        padding-inline: ${inputValues[0]}px;
+        padding-block: ${inputValues[1]}px;
+        background: #974ec2;
+        box-shadow: 0px 3px 0px 0px #513f99;
+        border-radius: ${inputValues[2]}px;
+        color: #fff;
+        font-size: ${inputValues[3]}px;
+        transition: all 50ms ease-in-out;
     }
 
     // hover style
@@ -31,7 +38,7 @@ export default function CodeOutput() {
                 <li className={styles.LanguageOptions}>
                     <ButtonPrimary btnLabel="HTML"></ButtonPrimary>
                     <ButtonPrimary
-                        btnLabel="css"
+                        btnLabel="CSS"
                         isActive={true}
                     ></ButtonPrimary>
                     <ButtonPrimary btnLabel="JavaScript"></ButtonPrimary>
--- src/components/SnippetEditor/SnippetOutput/SnippetOutput.jsx
+++ src/components/SnippetEditor/SnippetOutput/SnippetOutput.jsx
@@ -1,13 +1,32 @@
+// context
+import { useSnippetEdit } from "../../../SnippetEditorContext";
+
+// styles
 import styles from "./SnippetOutput.module.scss";
 
 export default function SnippetOutput() {
+    const inputValues = useSnippetEdit();
+
     return (
         <section className={styles.SnippetOutput}>
             <h3 className={styles.SnippetLabel}>
                 ID <span className={styles.SnippetID}>001</span>
             </h3>
             <div className={styles.SnippetDisplay}>
-                <a style={{}}>btn</a>
+                <a
+                    style={{
+                        paddingInline: `${inputValues[0]}px`,
+                        paddingBlock: `${inputValues[1]}px`,
+                        background: "#974ec2",
+                        boxShadow: "0px 3px 0px 0px #513f99",
+                        borderRadius: `${inputValues[2]}px`,
+                        color: "#fff",
+                        fontSize: `${inputValues[3]}px`,
+                        transition: "all 50ms ease-in-out",
+                    }}
+                >
+                    Click
+                </a>
             </div>
         </section>
     );
