175b74037df18f49c3cf40c10bcc476d6f530b94
Austin Chiatto <chiattoaustin@gmail.com>
2023-07-20 09:07:40 -0700

    hooked up box shadow options

--- src/components/CodeToImg/Sidebar/OptionButton/OptionButton.jsx
+++ src/components/CodeToImg/Sidebar/OptionButton/OptionButton.jsx
@@ -1,15 +1,16 @@
 // styles
 import styles from "./OptionButton.module.scss";
 
-export default function OptionButton({ index, icon, background, customStyle, isActive }) {
+export default function OptionButton({ index, icon, background, customStyle, isActive, updateInput }) {
     return (
         <li
             className={styles.Option}
             id={index}
             style={{
-                background: isActive ? "#e5eff9" : `${background}`,
+                background: background ? `${background}` : "",
                 border: isActive ? "0.125rem solid #63adf2" : "0.0625rem solid #DEDEE0",
             }}
+            onClick={() => updateInput(index)}
         >
             {icon ? (
                 <div
